06d7ba05f7c0ac7a8b80f1f1c7d104e3829a6369
Ionut <ionutzbalazs@hotmail.com>
2018-04-14 19:28:07 +0300

    compute chart on map click

--- assets/js/homeMap.js
+++ assets/js/homeMap.js
@@ -1,11 +1,20 @@
+var marker = null;
+
 function initMap() {
   var point = {lat: 46.773024, lng: 23.589494};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 15,
     center: point
   });
-  var marker = new google.maps.Marker({
-    position: point,
-    map: map
+  
+  map.addListener('click', function (event) {
+    if (marker) {
+      marker.setMap(null);
+    }
+    marker = new google.maps.Marker({
+      position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+      map: map
+    });
+    computePotentialOnDemand(event.latLng.lat(), event.latLng.lng());
   });
 }
